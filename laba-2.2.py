20847d50a4dae2bba7459509aa7689402f7c190c
mrSvinka <181984796+mrSvinka@users.noreply.github.com>
2025-02-07 01:43:26 +0200

    Add files via upload
    add

--- laba-2.2.py
+++ laba-2.2.py
@@ -1,7 +1,46 @@
 n = int(input("число n: "))
+d = 1
+u = 0
+r = 0
+y = 1
+h = 0
+#нашли число для целочисленного деления
+while u <= n:
+    if n // d >= 10:
+        d *= 10
+    else:
+        if 0 < n // d < 10:
+            u = d
+            break
+print(u)
+# нашли кол-во нулей в числе
+while u>=y:
+    if u // y >= 10:
+        y *= 10
+        r += 1
+    else:
+        if 0 < u // y < 10:
+            r += 0
+            break
+print(r, y)
+
+
 
 for i in range(n, 0, -1):
-    print(' ' * (n - i), end='')
+    while i>0:
+        if 0 >= i // u:
+            u /= 10
+            r -= 1
+
+        else:
+
+            if 0 < i // u < 10:
+                h+=r
+                print((' ' * (n + r - i)), end='')
+
+
+
+
 
     for j in range(i, 0, -1):
         print(j, end='')
@@ -10,3 +49,6 @@ for i in range(n, 0, -1):
         print(e, end='')
 
     print()
+
+
+
