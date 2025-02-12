4b149d32931374ba743de0834e1c1b694a814caf
mrSvinka <181984796+mrSvinka@users.noreply.github.com>
2025-02-12 10:03:30 +0200

    Add files via upload
    ADD

--- laba-2.2.py
+++ laba-2.2.py
@@ -4,6 +4,7 @@ u = 0
 r = 0
 y = 1
 h = 0
+
 #нашли число для целочисленного деления
 while u <= n:
     if n // d >= 10:
@@ -12,43 +13,34 @@ while u <= n:
         if 0 < n // d < 10:
             u = d
             break
-print(u)
-# нашли кол-во нулей в числе
+
+# нашли кол-во нулей в числе (для послед. прибавления пропуска)
 while u>=y:
     if u // y >= 10:
         y *= 10
         r += 1
     else:
         if 0 < u // y < 10:
-            r += 0
             break
-print(r, y)
 
-
-
+#формеруем отступы
 for i in range(n, 0, -1):
-    while i>0:
-        if 0 >= i // u:
-            u /= 10
-            r -= 1
+    if 0 >= i // u:
+        u /= 10
+        r -= 1
+        print((' ' * ((n + h) - i)), end='')
+    else:
+        if 0 < i // u < 10:
+            h += r
+            print((' ' * ( (n+h) - i)), end='')
 
-        else:
-
-            if 0 < i // u < 10:
-                h+=r
-                print((' ' * (n + r - i)), end='')
-
-
-
-
-
+# Формирование первой части - от i до 1
     for j in range(i, 0, -1):
         print(j, end='')
 
-    for e in range(2, i + 1):
-        print(e, end='')
+# Формирование второй части - от 2 до i
+    for j in range(2, i + 1):
+        print(j, end='')
 
+# Переход на следующую строку
     print()
-
-
-
